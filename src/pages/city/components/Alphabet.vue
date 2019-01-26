9a5c1b3e78b1140ce7118bca1bdd33969172c552
lwy-lenovo <527563266@qq.com>
2019-01-26 19:24:31 +0800

    finish city list css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/global.scss';
+.list{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    right:0;
+    top:rem(75px);
+    bottom:0;
+    width:rem(20px);
+    .item{
+        line-height:rem(17px);
+        text-align: center;
+        color:$bgColor;
+    }
+}
+</style>
+
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="city-list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/global.scss';
+.border-topbottom{
+    &:before{
+        border-color: #ccc;
+    }
+    &:after{
+        border-color: #ccc;
+    }
+}
+.border-bottom{
+    &:before{
+        border-color: #ccc;
+    }
+}
+.city-list{
+    overflow: hidden;
+    position: absolute;
+    top:rem(75px);
+    left:0;
+    right:0;
+    bottom:0;
+    .title{
+        line-height:rem(30px);
+        background: #eee;
+        padding-left:rem(10px);
+        color:#666;
+        font-size:(13px);
+    }
+    .button-list{
+        padding:rem(5px) rem(30px) rem(5px) rem(5px);
+        overflow: hidden;
+        .button-wrapper{
+            float:left;
+            width:33.33%;
+            .button{
+                padding:rem(5px) 0;
+                text-align: center;
+                margin:rem(5px);
+                border:rem(1px) solid #ccc;
+                border-radius: rem(3px);
+            }
+        }
+    }
+    .item-list{
+        .item{
+            line-height:rem(15px);
+            color:#666;
+            padding:rem(10px);
+        }
+    }
+}
+</style>
+
