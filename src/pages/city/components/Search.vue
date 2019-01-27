ccfec76f255661a51f4058c058bbc164d56e80fd
lwy-lenovo <527563266@qq.com>
2019-01-27 12:13:14 +0800

    finish search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,61 @@
 <template>
     <div class="city-search">
-        <input type="text" class="search-input" placeholder="输入城市或拼音" />
+        <div class="action-search">
+            <input type="text" v-model="keyword" class="search-input" placeholder="输入城市或拼音" />
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+                <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data() {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (const i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -24,6 +73,23 @@ export default {
         color:#666;
         box-sizing: border-box;
         padding: 0 rem(10px);
+    }
+    .search-content{
+        overflow: hidden;
+        position: absolute;
+        top:rem(75px);
+        left:0;
+        right:0;
+        bottom:0;
+        background: eee;
+        z-index:1;
+        .search-item{
+            line-height:rem(30px);
+            padding-left:rem(10px);
+            background: #fff;
+            color:#666;
+
+        }
     }
 }
 </style>
