aa70db208483f5198d527defe5f556e51722009f
linwenyao <linwenyao@flytexpress.com>
2019-01-30 15:28:32 +0800

    update

--- config/index.js
+++ config/index.js
@@ -11,8 +11,14 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api': {
-        target: 'http://localhost:8080',
+      // '/api': {// 开发
+      //   target: 'http://localhost:8080',
+      //   pathRewrite: {
+      //     '^/api': '/static/mock'
+      //   }
+      // },
+      '/api': {// 打包上线
+        target: 'https://ll527563266.github.io/Travel',
         pathRewrite: {
           '^/api': '/static/mock'
         }
